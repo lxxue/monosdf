[09-08] remove gt scaling in depth loss; fix mask and sampling_idx in dataset; improve logging

code/model/loss.py
@@ -187,7 +187,7 @@ class MonoSDFLoss(nn.Module):
     
     def get_depth_loss(self, depth_pred, depth_gt, mask):
         # TODO remove hard-coded scaling for depth
-        return self.depth_loss(depth_pred.reshape(1, 32, 32), (depth_gt * 50 + 0.5).reshape(1, 32, 32), mask.reshape(1, 32, 32))
+        return self.depth_loss(depth_pred.reshape(1, 32, 32), (depth_gt).reshape(1, 32, 32), mask.reshape(1, 32, 32))
         
     def get_normal_loss(self, normal_pred, normal_gt):
         normal_gt = torch.nn.functional.normalize(normal_gt, p=2, dim=-1)
@@ -243,11 +243,11 @@ class MonoSDFLoss(nn.Module):
         output = {
             'loss': loss,
             'rgb_loss': rgb_loss,
-            'eikonal_loss': eikonal_loss,
-            'smooth_loss': smooth_loss,
-            'depth_loss': depth_loss,
-            'normal_l1': normal_l1,
-            'normal_cos': normal_cos
+            'eikonal_loss': self.eikonal_weight * eikonal_loss,
+            'smooth_loss': self.smooth_weight * smooth_loss,
+            'depth_loss': decay * self.depth_weight * depth_loss,
+            'normal_l1': decay * self.normal_l1_weight * normal_l1,
+            'normal_cos': decay * self.normal_cos_weight * normal_cos
         }
 
         return output

code/sdf_datasets/v2a_dataset.py
@@ -138,7 +138,7 @@ class SceneDatasetDN(torch.utils.data.Dataset):
                  num_views=-1
                  ):
 
-        self.instance_dir = os.path.join("../data/v2a", data_dir, "processed")
+        self.instance_dir = os.path.join("/media/lixin/DATA/lixin/datasets/human-scene-recon/record3d", data_dir, "processed")
 
         self.total_pixels = img_res[0] * img_res[1]
         self.img_res = img_res
@@ -241,7 +241,11 @@ class SceneDatasetDN(torch.utils.data.Dataset):
             for path in mask_paths:
                 mask = imageio.imread(path)
                 # mask = np.load(path)
-                self.mask_images.append(torch.from_numpy(mask.reshape(-1, 1)).float())
+                mask = torch.from_numpy(mask.reshape(-1, 1)).float() / 255.0
+                # print(mask.min(), mask.max(), mask.mean())
+                mask = 1 - mask
+                # print(mask.min(), mask.max(), mask.mean())
+                self.mask_images.append(mask)
 
     def __len__(self):
         return self.n_images
@@ -269,6 +273,12 @@ class SceneDatasetDN(torch.utils.data.Dataset):
         }
 
         if self.sampling_idx is not None:
+            num_pixel = 1024
+            sampling_idx = torch.randperm(self.total_pixels)[:num_pixel * 3] 
+            mask = self.mask_images[idx][sampling_idx, 0]
+            sampling_idx = sampling_idx[mask > 0]
+            sampling_idx = sampling_idx[:num_pixel]
+            self.sampling_idx = sampling_idx
             ground_truth["rgb"] = self.rgb_images[idx][self.sampling_idx, :]
             ground_truth["full_rgb"] = self.rgb_images[idx]
             ground_truth["normal"] = self.normal_images[idx][self.sampling_idx, :]

code/training/exp_runner.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     parser.add_argument('--scan_id', type=int, default=-1, help='If set, taken to be the scan id.')
     parser.add_argument('--cancel_vis', default=False, action="store_true",
                         help='If set, cancel visualization in intermediate epochs.')
-    parser.add_argument("--local_rank", type=int, required=True, help='local rank for DistributedDataParallel')
+    # parser.add_argument("--local_rank", type=int, required=True, help='local rank for DistributedDataParallel')
 
     opt = parser.parse_args()
 
@@ -50,11 +50,13 @@ if __name__ == '__main__':
         world_size = -1
 
     # print(opt.local_rank)
-    local_rank = int(os.environ['LOCAL_RANK'])
-    gpu = local_rank
-    print("local rank: ", local_rank)
+    # local_rank = int(os.environ['LOCAL_RANK'])
+    # gpu = local_rank
+    # print("local rank: ", local_rank)
     # torch.cuda.set_device(opt.local_rank)
-    torch.cuda.set_device(local_rank)
+    # torch.cuda.set_device(local_rank)
+    gpu=0
+    torch.cuda.set_device(0)
     torch.distributed.init_process_group(backend='nccl', init_method='env://', world_size=world_size, rank=rank, timeout=datetime.timedelta(1, 1800))
     torch.distributed.barrier()
 